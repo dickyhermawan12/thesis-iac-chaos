e628939e95f43b5dda2a101c68d5ecf1189a0586
dicky <52572645+dickyhermawan12@users.noreply.github.com>
2023-06-14 06:53:52 +0700

    add gunicorn service root

--- iac/live/modules/vmss/custom-data/app-vmss.sh
+++ iac/live/modules/vmss/custom-data/app-vmss.sh
@@ -21,3 +21,49 @@ sudo apt install nginx -y
 sudo rm /etc/nginx/sites-enabled/default
 sudo cp /home/dicky/codebase/backend/nginx.conf /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
+
+#!/bin/bash
+fallocate -l 1G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "===> Swapfile created" >~/log.txt
+cd ~
+git clone https://github.com/dickyhermawan12/thesis-iac-chaos.git codebase
+cd codebase/backend
+DB_DRIVER="mysql"
+DB_HOST="localhost"
+DB_PORT="3306"
+DB_NAME="microblog"
+DB_USER="root"
+DB_PASS="mysqlpass"
+SSL_CA="thesisdb.pem"
+SECRET_KEY="thesissecretkey"
+ALGORITHM="HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES="150"
+cat <<EOF >.env
+DB_DRIVER=$DB_DRIVER
+DB_HOST=$DB_HOST
+DB_PORT=$DB_PORT
+DB_NAME=$DB_NAME
+DB_USER=$DB_USER
+DB_PASS=$DB_PASS
+SSL_CA=$SSL_CA
+SECRET_KEY=$SECRET_KEY
+ALGORITHM=$ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES=$ACCESS_TOKEN_EXPIRE_MINUTES
+EOF
+echo "===> Installing dependencies" >>~/log.txt
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+echo "===> Setup gunicorn" >>~/log.txt
+pip install gunicorn httptools uvloop
+cp ~/codebase/backend/gunicorn.service.root /etc/systemd/system/gunicorn.service
+systemctl daemon-reload
+systemctl start gunicorn
+systemctl enable gunicorn
+echo "===> Setup nginx" >>~/log.txt
+cp ~/codebase/backend/nginx.conf /etc/nginx/sites-enabled/default
+systemctl restart nginx
+echo "===> Setup completed" >>~/log.txt
--- iac/live/modules/vmss/custom-data/web-vmss.sh
+++ iac/live/modules/vmss/custom-data/web-vmss.sh
@@ -21,26 +21,29 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo cp /home/dicky/codebase/frontend/nginx.conf /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
 
+#!/bin/bash
 fallocate -l 1G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
-echo "===> Swapfile created"
+echo "===> Swapfile created" >~/log.txt
 cd ~
 git clone https://github.com/dickyhermawan12/thesis-iac-chaos.git codebase
 cd codebase/frontend
 echo "NEXT_PUBLIC_BACKEND_URL=http://localhost:5000" >.env.local
 echo "NEXTAUTH_SECRET=thesissecretkey" >>.env.local
-echo "===> Installing dependencies and building the app"
-npm install
-npm run build
-echo "===> Setup pm2"
-pm2 start npm --name "microblog" -- start
-pm2 startup
-pm2 save
-echo "===> Setup nginx"
-rm /etc/nginx/sites-enabled/default
-# replace BACKEND_IP_ADDRESS with actual IP address of 15.1.3.5 the backend in nginx.conf
-sed -i 's/BACKEND_IP_ADDRESS/15.1.3.5/g' /home/dicky/codebase/frontend/nginx.conf
-cp /home/dicky/codebase/frontend/nginx.conf /etc/nginx/sites-enabled/default
+echo "===> Installing dependencies and building the app" >>~/log.txt
+npm install >~/trail.txt 2>&1
+npm run --silent build >>~/trail.txt 2>&1
+echo "===> Setup pm2" >>~/log.txt
+HOME="/root"
+export HOME
+sudo -E pm2 list >>~/trail.txt 2>&1
+sudo -E pm2 start npm --name "microblog" -- start >>~/trail.txt 2>&1
+sudo -E pm2 startup >>~/trail.txt 2>&1
+sudo -E pm2 save
+echo "===> Setup nginx" >>~/log.txt
+sed -i 's/BACKEND_IP_ADDRESS/15.1.3.5/g' ~/codebase/frontend/nginx.conf
+cp ~/codebase/frontend/nginx.conf /etc/nginx/sites-enabled/default
 systemctl restart nginx
+echo "===> Setup completed" >>~/log.txt
